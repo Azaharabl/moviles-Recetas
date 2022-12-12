998255e1f4cf28a227af30af3d65f1ddf1b3a6c3
Azaharabr <blancaluna92@hotmail.com>
2022-12-12 10:26:40 +0100

    Añadir en el main el navegador controller

--- RecetasDeAzahara/app/src/main/java/com/example/recetasdeazahara/MainActivity.kt
+++ RecetasDeAzahara/app/src/main/java/com/example/recetasdeazahara/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.recetasdeazahara
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    //para la navegacion
+    lateinit var  navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //iniciar la navegacion
+        navController = findNavController(R.id.fragmentContainerView)
+        setupActionBarWithNavController(navController)
+
+
     }
+
+
+
 }
